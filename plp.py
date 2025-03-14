33ecfd80b023e7a7a5d10cf1d43329b9aecfe0a8
andyweirdo7 <JCode-7@users.noreply.github.com>
2025-03-14 18:47:22 +0300

    Web1

---
+++ plp.py
@@ -0,0 +1,10 @@
+number = 10
+print ("Hello world")
+print (number)
+
+my_name = "Jessie Jackson"
+print (my_name)
+
+languages = ["teamo", "leamo", "neamo", 123]
+print (languages)
+print (languages [2])
